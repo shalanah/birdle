c095981c0d57c17d3d15375ee91982ba3b2816b0
shalanah <shalanahfaith@gmail.com>
2022-08-16 19:35:33 -0500

    Add styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  *, *:before, *:after {
+    box-sizing: border-box;
+    margin: 0px;
+    padding: 0px;
+  }
+  html {
+    background: #222;
+    color: #fff;
+  }
+  body, html, #root {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
 function App() {
-  return <div className="App">Hey</div>;
+  return (
+    <div className="App">
+      <GlobalStyle />
+      Hey
+    </div>
+  );
 }
 
 export default App;
