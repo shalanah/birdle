8aaffcc7c06693d57d739a6a8db659a0ea80864f
shalanah <shalanahfaith@gmail.com>
2022-08-20 00:41:55 -0500

    Small responsive changes

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,9 +1,28 @@
+/// <reference types="vite-plugin-svgr/client" />
 import React from "react";
 import { getLetterStatusKeyboard, bgKeyboard, keysArray } from "./utils";
 import styled from "styled-components";
 import { ReactComponent as Delete } from "./assets/backspace.svg";
 import useGameContext from "./hooks/useGameContext";
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 30px;
+  gap: 8px;
+  margin-bottom: 8px;
+  @media (max-width: 768px) {
+    gap: 4px;
+  }
+`;
+const Row = styled.div`
+  margin: 0 auto;
+  display: flex;
+  gap: 6px;
+  @media (max-width: 768px) {
+    gap: 4px;
+  }
+`;
 const Btn = styled.button`
   border: none;
   padding: 0 10px;
@@ -17,26 +36,19 @@ const Btn = styled.button`
   font-weight: bold;
   transition: background 0.2s 1.4s; /* delay so we can see flip animation first */
   @media (max-width: 768px) {
-    min-width: 30px;
-    height: 40px;
+    min-width: 28px;
+    height: 48px;
+    padding: 0 7px;
   }
 `;
 
 function Keyboard() {
   const { attempts, actual, onKey } = useGameContext();
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        marginTop: 30,
-        gap: 8,
-        marginBottom: 8,
-      }}
-    >
+    <Container>
       {keysArray.map((row, i) => {
         return (
-          <div key={i} style={{ margin: "0 auto", display: "flex", gap: 6 }}>
+          <Row key={i}>
             {row.map((letter) => {
               const status = getLetterStatusKeyboard(attempts, actual, letter);
               return (
@@ -55,10 +67,10 @@ function Keyboard() {
                 </Btn>
               );
             })}
-          </div>
+          </Row>
         );
       })}
-    </div>
+    </Container>
   );
 }
 
